2c527550eed0062f6d3628830db964a8ed2e1aaa
sarababe1318 <240755@utxicotepec.edu.mx>
2026-01-07 09:45:41 -0600

    Practica02
    add: se agrego el archivo del ejercicio 01 - Declaracion de variables, aun sin terminar

---
+++ js/ejercicio01.js
@@ -0,0 +1,16 @@
+// 1. Declaracion de variables utilizando el prefijo VAR
+var miNombre = "Blanca Sarahi"
+var misApellidos;
+
+//Utilizaremos la funcion console.log para imprimir el valor actual de la variable
+console.log("El valor de la variable miNombre es =", miNombre);
+console.log("El valor de la variable misApellidos es =", misApellidos);
+//este arroja un undefined dado que no ha  sido inicializada, osea no se le asigna un valor
+
+misApellidos ="Melendez Torres"
+//la variable ya tiene un valor almacenado poir que al imprimir debe mostrar el dato
+console.log("El valor de la variable misApellidos es =", misApellidos);
+
+//para actualuizar el valor de una variable previamente definida solo basta con igualar el nuevi valor 
+miNombre= "Blanca Sarahi";
+console.log ("Hola ,", miNombre, misApellidos)
